16f889a547cc3815d7ffd3a3d8e8f31876b78d7f
Nathishwar-prog <nathishwarc@gmail.com>
2025-11-16 15:37:51 +0530

    Updated the issues By CORS

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // For Netlify deployment, we need to use the correct API URL
 const API = axios.create({ 
-  baseURL: import.meta.env.VITE_API_URL || 'https://todoapp-olaw.onrender.com/api' 
+  baseURL: import.meta.env.VITE_API_URL || 'https://backendtodo-nk7m.onrender.com/api' 
 });
 
 export default API;
